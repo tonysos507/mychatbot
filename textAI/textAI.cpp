ff8367142a173493509ed60c5aceca8677cde31f
Tonysos507 <sos507@163.com>
2026-09-23 21:30:54 -0700

    add textAI

---
+++ textAI/textAI.cpp
@@ -0,0 +1,24 @@
+#define NOMINMAX
+#include <torch/torch.h>
+
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+int main()
+{
+	int ret = 0;
+
+
+
+	return ret;
+}
